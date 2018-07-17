f3bc6a7ca5880016515d98359955c7d4b6e6a7e3
Richard Hidalgo <hidalgo931@gmail.com>
2018-07-16 20:35:29 -0400

    add style files

---
+++ src/components/styles.js
@@ -0,0 +1,10 @@
+export const FONT_COLOR = '#555';
+export const FONT_SIZE = 20;
+
+export const CELL_LIVE_COLOR = '#D32F2F';
+export const CELL_EMPTY_COLOR = '#FFCDD2';
+export const CELL_BORDER_COLOR = '#FFFFFF';
+export const PATTERN_BORDER_COLOR = '#272727';
+export const TRANSPARENT_COLOR = 'rgba(0, 0, 0, 0)';
+
+export const CELL_BORDER_SIZE = 4;
