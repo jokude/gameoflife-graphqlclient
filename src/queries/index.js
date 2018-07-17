11ee9254eccda3eb7651ee6357168a3abef00aa7
Richard Hidalgo <hidalgo931@gmail.com>
2018-07-16 20:31:22 -0400

    remove queries

--- src/queries/index.js
+++
@@ -1,3 +0,0 @@
-export { default as getBoard } from './getBoard';
-export { default as getMessages } from './getMessages';
-export { default as putPattern } from './putPattern';
--- src/queries/putPattern.js
+++
@@ -1,7 +0,0 @@
-import gql from 'graphql-tag';
-
-export default gql`
-  mutation putPattern($positions: [Int]) {
-    putPattern(positions: $positions)
-  }
-`;
