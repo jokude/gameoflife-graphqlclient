88e1c33a4de832433b9c737e8d624ee0a52f5b54
Richard Hidalgo <hidalgo931@gmail.com>
2018-08-10 00:05:12 -0400

    move styles to constants

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,18 @@
-export const CELL_SIZE = 20;
+// Names
 export const GRID_NAME = 'board-grid';
+
+// Sizes
+export const CELL_SIZE = 20;
+export const BOARD_SIZE = 40;
+export const CELL_BORDER_SIZE = 4;
+
+// Font
+export const FONT_SIZE = 20;
+export const FONT_COLOR = '#555';
+
+// Colors
+export const CELL_LIVE_COLOR = '#D32F2F';
+export const CELL_EMPTY_COLOR = '#FFCDD2';
+export const CELL_BORDER_COLOR = '#FFFFFF';
+export const PATTERN_BORDER_COLOR = '#272727';
+export const TRANSPARENT_COLOR = 'rgba(0, 0, 0, 0)';
